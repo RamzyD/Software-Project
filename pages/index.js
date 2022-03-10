1bbacb0273b2153d30f3840fe2ede4ed609c1041
RamzyD <93335425+RamzyD@users.noreply.github.com>
2022-03-10 09:18:25 +0000

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import React, { useContext } from "react";
+
+import { Context } from "../context";
+
+import { useRouter } from "next/router;"
+
+
+
+export default function Auth() {
+  return <div className="background">auth</div>;
+}
